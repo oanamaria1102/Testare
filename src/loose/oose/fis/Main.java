05753e3659086239ba71e92ae3cc98dcd232358b
MarioRivis <mario.rivis@gmail.com>
2019-02-26 14:01:51 +0200

    New Main.java for sequence diagram

--- src/loose/oose/fis/Main.java
+++ src/loose/oose/fis/Main.java
@@ -20,22 +20,31 @@ public class Main {
         Document xml = new XML(xmlList);
 
 
-        String[] jsonList = new String[2];
-        jsonList[0] = "nume:";
-        jsonList[1] = "valoare1";
+        String[] xmlList = new String[6];
+        xmlList[0] = "<tag1>";
+        xmlList[1] = "text1";
+        xmlList[2] = "</tag1>";
+        xmlList[3] = "<tag2>";
+        xmlList[4] = "text2";
+        xmlList[5] = "</tag2>";
 
-        Document json = new JSON(jsonList);
+        Document xml = new XML(xmlList);
 
         ArrayList<Document> documente = new ArrayList<>();
         documente.add(xml);
-        documente.add(json);
 
-        Procesor       procesorCautare  = new ProcesorCautare("valoare");
-        Procesor       procesorCautare1 = new ProcesorCautare("valoare1");
-        ProcesorCompus procesorCompus   = new ProcesorCompus();
-        procesorCompus.adaugaProcesor(procesorCautare);
-        procesorCompus.adaugaProcesor(procesorCautare1);
+        Procesor c1 = new ProcesorCautare("text1");
+        Procesor c2 = new ProcesorCautare("text3");
+        Procesor c3 = new ProcesorCautare("text2");
 
-        System.out.println(procesorCompus.proceseaza(documente));
+        ProcesorCompus pc1 = new ProcesorCompus();
+        pc1.adaugaProcesor(c1);
+        pc1.adaugaProcesor(c2);
+
+        ProcesorCompus pc2 = new ProcesorCompus();
+        pc2.adaugaProcesor(pc1);
+        pc2.adaugaProcesor(c3);
+
+        System.out.println(pc2.proceseaza(documente));
     }
 }
